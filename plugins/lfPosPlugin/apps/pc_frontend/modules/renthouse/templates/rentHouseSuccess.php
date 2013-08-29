bf045cede9015c0e2d2f9c379a2ebe8caf868f04
muoidv <muoi.dvm@gmail.com>
2013-08-29 20:23:57 +0700

    style nav left menu

--- plugins/lfPosPlugin/apps/pc_frontend/modules/renthouse/templates/rentHouseSuccess.php
+++ plugins/lfPosPlugin/apps/pc_frontend/modules/renthouse/templates/rentHouseSuccess.php
@@ -1,13 +1,9 @@
 <div id="leftCol">
     <div id="left-col-nav" class="clearfix">
         <ul>
-            <li id="find-house"  class="first-list-item"><a href="" title="Tìm kiếm nhà cho thuê" id="a-find-house">Tìm nhà</a></li>
-            <li class="division">|</li>
-            <li id="post-house"><a href="" id="a-post-house" title="Đăng tin nhà cho thuê">Đăng nhà</a></li>
-            <li class="division">|</li>
-            <li id="my-house"><a href="" id="a-my-house" title="Quản lý nhà của tôi">Nhà của tôi</a></li>
-            <li class="division">|</li>
-            <li id="follow"><a href="" id="a-follow" title="Danh sách nhà tôi đang theo dõi">Theo dõi</a></li>
+            <li id="find-house"  class="first-list-item"><a class="kd-button" href="" title="Tìm kiếm nhà cho thuê" id="a-find-house">Tìm nhà</a></li>
+            <li id="post-house"><a class="kd-button" href="" id="a-post-house" title="Đăng tin nhà cho thuê">Đăng nhà</a></li>
+            <li id="follow"><a class="kd-button" href="" id="a-follow" title="Danh sách nhà tôi đang theo dõi">Theo dõi</a></li>
         </ul>
     </div>
     <div id="add-my-new-place">
